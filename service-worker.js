2cc125ae7a143c3ae9498b468149f734a874148e
Maxel-Uds <maxellopes32@gmail.com>
2024-10-27 19:58:20 -0300

    ajuste na instalação

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,15 @@
-const CACHE_NAME = 'despesas-cache-v1';
-const urlsToCache = [
-  '/',
-  '/css/style.css',
-  '/js/app.js',
-  '/index.html'
-];
-
-self.addEventListener('install', function(event) {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        return cache.addAll(urlsToCache);
-      })
+    caches.open('pwa-cache').then((cache) => {
+      return cache.addAll(['/']);
+    })
   );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        return response || fetch(event.request);
-      })
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
